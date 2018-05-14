0494678317c44c5309cc198c9f50dd92494b9726
Tony Ho <ho.tonym@gmail.com>
2018-05-14 21:09:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,7 +36,7 @@ letters_array = Array.new
     letter[2] = "$"
   end
 
-  kesha_array = letters_array.each do |element|
+  kesha_array = array.each do |element|
     element.join
   end
   kesha_array
