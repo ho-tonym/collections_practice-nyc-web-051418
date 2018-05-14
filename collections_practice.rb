74cb08f4cc95a8ef46d53d1162930e24975d0f62
Tony Ho <ho.tonym@gmail.com>
2018-05-14 20:51:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,10 +14,9 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array[1] = second
-  array[2] = third
+ second = array[1]
+ third = array[2]
 
-  binding.pry
   array[1] = third
   array[2] = second
   array
